b778f0aec9a94a8f756b3efef9c7ce3cd7681e22
Anandan2257 <periasamy312004@gmail.com>
2025-01-19 01:05:55 +0530

    first commit

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
+
+const About = () => {
+  return (
+    <div>
+
+      <div className='text-2xl text-center pt-8 border-t'>
+          <Title text1={'ABOUT'} text2={'US'} />
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+          <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+          <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+              <p>One Village Functions was born to bridge the gap between Tamil Nadu's vibrant cultural traditions and the modern needs of our people. </p>
+              <p>Our platform is a one-stop destination for connecting with local vendors and services, ensuring every event is celebrated with authenticity and ease.</p>
+              <b className='text-gray-800'>Our Mission</b>
+              <p>Our mission at Forever is to empower customers with choice, convenience, and confidence. We're dedicated to providing a seamless shopping experience that exceeds expectations, from browsing and ordering to delivery and beyond.</p>
+          </div>
+      </div>
+
+      <div className=' text-xl py-4'>
+          <Title text1={'WHY'} text2={'CHOOSE US'} />
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Quality Assurance:</b>
+            <p className=' text-gray-600'>We partner with verified vendors to ensure you receive the best services and products for your events.</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Convenience:</b>
+            <p className=' text-gray-600'>Browse, compare, and book services or products with just a few clicks, all in one app.</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Exceptional Customer Service:</b>
+            <p className=' text-gray-600'>Our dedicated support team is always available to assist you, ensuring your satisfaction is our top priority.</p>
+          </div>
+      </div>
+
+      <NewsletterBox/>
+      
+    </div>
+  )
+}
+
+export default About
