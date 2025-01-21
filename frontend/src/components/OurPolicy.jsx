f619c58628add0a33c92c36311109d4dc509d3f2
Anandan2257 <periasamy312004@gmail.com>
2025-01-21 12:17:08 +0530

    change

--- frontend/src/components/OurPolicy.jsx
+++ frontend/src/components/OurPolicy.jsx
@@ -13,12 +13,12 @@ const OurPolicy = () => {
       <div>
         <img src={assets.quality_icon} className='w-12 m-auto mb-5' alt="" />
         <p className=' font-semibold'>Satisfaction Guarantee        </p>
-        <p className=' text-gray-400'>Experience our 7-day satisfaction guarantee: if you're not happy, we're not happy!        </p>
+        <p className=' text-gray-400'>if you're not happy, we're not happy!        </p>
       </div>
       <div>
         <img src={assets.support_img} className='w-12 m-auto mb-5' alt="" />
         <p className=' font-semibold'>Dedicated Support        </p>
-        <p className=' text-gray-400'>Get help whenever you need it: our friendly support team is available 24/7</p>
+        <p className=' text-gray-400'>our friendly support team is available 24/7</p>
       </div>
 
     </div>
