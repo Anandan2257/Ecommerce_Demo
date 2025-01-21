79119b88526e96660ebfeb00f27cfc057104f1ea
Anandan2257 <periasamy312004@gmail.com>
2025-01-21 12:34:11 +0530

    change

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -29,15 +29,15 @@ const About = () => {
       <div className='flex flex-col md:flex-row text-sm mb-20'>
           <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
             <b>Quality Assurance:</b>
-            <p className=' text-gray-600'>We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
+            <p className=' text-gray-600'>We carefully select and vet each service provider to ensure they meet our high standards.            </p>
           </div>
           <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
             <b>Convenience:</b>
-            <p className=' text-gray-600'>With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p>
+            <p className=' text-gray-600'>Our platform offers a seamless experience, making it easy to find and access the services you need.            </p>
           </div>
           <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
             <b>Exceptional Customer Service:</b>
-            <p className=' text-gray-600'>Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is our top priority.</p>
+            <p className=' text-gray-600'>Our dedicated team is here to assist you every step of the way, ensuring your satisfaction is our top priority.</p>
           </div>
       </div>
 
