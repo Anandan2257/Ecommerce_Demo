259e22db399233476414803c973def7d47d1dc53
Anandan2257 <periasamy312004@gmail.com>
2025-01-23 22:33:36 +0530

    change

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -14,10 +14,10 @@ const Contact = () => {
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
         <img className='w-full md:max-w-[350px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
-          <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+          <p className='font-semibold text-xl text-gray-600'>Our Company</p>
           <p className=' text-gray-500'>3/113 Koolipatti <br /> Namakkal TamilNadu - 637002</p>
           <p className=' text-gray-500'>+91 6374555018<br /> Email: admin@example.com</p>
-          <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
+          <p className='font-semibold text-xl text-gray-600'>Careers at One app One Village</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
