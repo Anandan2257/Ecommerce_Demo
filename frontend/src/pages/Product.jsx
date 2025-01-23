8f481dd397d22002ae96572ce89b54be98bc4f20
Anandan2257 <periasamy312004@gmail.com>
2025-01-23 15:52:37 +0530

    change

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -82,7 +82,7 @@ const Product = () => {
       <div className='mt-20'>
         <div className='flex'>
           <b className='border px-5 py-3 text-sm'>Description</b>
-          <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+          <p className='border px-5 py-3 text-sm'>Reviews (1011)</p>
         </div>
         <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
         <p>Welcome to our village services platform, where convenience meets community. Our online hub connects villagers with essential services, fostering a sense of connection and accessibility.</p>
